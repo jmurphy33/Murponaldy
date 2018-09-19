52ac6dbeb8e1afeae164b67ff4b91283d5a8d041
Jason Murphy <jmurphy3737@outlook.com>
2018-09-19 23:35:00 +0100

    Here's Harry Potter

--- react-node/src/App.js
+++ react-node/src/App.js
@@ -3,19 +3,66 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
+
+const books = [
+  { id: 0, title: "Harry Potter 1", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 1, title: "Harry Potter 2", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 2, title: "Harry Potter 3", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 3, title: "Harry Potter 4", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 4, title: "Harry Potter 5", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 5, title: "Harry Potter 6", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 6, title: "Lord of The Rings 1", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 7, title: "Lord of The Rings 2", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 8, title: "Lord of The Rings 3", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 9, title: "Game of Thrones 1", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 10, title: "Game of Thrones 2", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 11, title: "Game of Thrones 3", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 12, title: "Game of Thrones 4", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 13, title: "Game of Thrones 5", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 14, title: "Game of Thrones 6", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 15, title: "Game of Thrones 7", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 16, title: "Goosebumps 1", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 17, title: "Goosebumps 2", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 18, title: "Goosebumps 3", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 19, title: "Goosebumps 3", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 20, title: "Marvel 1", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 21, title: "DC 2", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 22, title: "IT", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 23, title: "The Shinning", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+  { id: 24, title: "Private Peaceful", published: "", author: "", description: "", genre: "", ageRestriction: "" },
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar /> 
-        <div className="row"> 
+        <Navbar />
+        <div className="row">
           <div className="col-lg-1 sideContent"></div>
-          <div className="col-lg-10 mainContent"> test</div>
+          <div className="col-lg-10 mainContent">
+
+            <div className="row">
+              <BooksDiv />
+            </div>
+
+          </div>
           <div className="col-lg-1 sideContent"></div>
         </div>
-      </div> 
+      </div>
     );
   }
+}
+
+function BooksDiv() {
+  var renderOutput = null;
+
+  renderOutput = books.map(item =>
+    <div className="col-lg-1 col-md-1 col-sm-1">
+      <label>{item.title}</label>
+    </div>
+  );
+
+  return (<span>{renderOutput}</span>)
 }
 
 function Navbar() {
